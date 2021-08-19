fdb77a8d5a63019a47a4850880079218eb547f23
anvox <thien.an.vo.nguyen@gmail.com>
2021-08-19 13:18:51 +0700

    Add text

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,10 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  # Meaning less text
+  # Meaning less text
+  # Meaning less text
+  # Meaning less text
+  # Meaning less text
+  # Meaning less text
 end
